38f6b0c362aafd94a6e2fed03c2434126a8b8cd0
devopstraining1990 <66471310+devopstraining1990@users.noreply.github.com>
2020-06-06 11:13:53 +0530

    Updated the Hello Java Class

--- Hello.java
+++ Hello.java
@@ -23,6 +23,7 @@ public class Hello{
 			System.out.println("Thanks for the webhooks demonstration on 05-jun-2020..");
 			System.out.println("demo on webhooks - 28");
 			
+			
 						System.out.println("demo on webhooks - suresh0523");
 			
 		}
